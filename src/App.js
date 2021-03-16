88ea59487e3cad11a3b9c8be35a37747004abd80
Weverton Pires <w.m.pires@hotmail.com>
2021-03-16 10:58:25 -0300

    ReactJS para Iniciantes: Tutorial Completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MyBody from './MyBody.js';
 
 function App() {
+  
+  const [str, setStr] = useState('Olá mundo');
+
+  function handleClick() {
+    setStr('Olá Danki Code');
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{str}</h2>
+      <MyBody conteudo="Body1"></MyBody>
+      <MyBody conteudo="Body2"></MyBody>
+      <MyBody conteudo="Body3"></MyBody>
     </div>
   );
 }
---
+++ src/MyBody.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyBody = (props) => {
+    return (
+        <h2>Meu componente Body {props.conteudo}!</h2>
+    );
+}
+
+export default MyBody;
